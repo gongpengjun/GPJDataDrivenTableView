3e8feae5dd1d4956718cce3148225e1f9bc9ba25
gongpengjun <frank.gongpengjun@gmail.com>
2018-12-04 22:27:34 +0800

    remove unnecessary pre-declaration

--- GPJDataDrivenTableView/GPJDataDrivenTableView.h
+++ GPJDataDrivenTableView/GPJDataDrivenTableView.h
@@ -6,8 +6,6 @@
 
 #import <UIKit/UIKit.h>
 
-@class GPJDataDrivenTableView;
-
 @interface GPJBaseData : NSObject
 @property (nonatomic, copy) void (^didSelectAction)(id data);
 - (CGFloat)cellHeight;
